1c8138ad6be9ee35d3d176e2256f8e9b416b9df3
Julián <j.gakusei11@gmail.com>
2022-07-21 20:56:14 -0300

    HTML de header y footer terminado

--- public/components/footer.js
+++ public/components/footer.js
@@ -8,11 +8,59 @@ class Footer extends HTMLElement
     connectedCallback() 
     {
       this.innerHTML = `
-        <style>
-          
-        </style>
-        <footer>
-            <h1>Aqui va el footer. Luego será actualizado.</h1>
+        <hr>
+        <footer class="footer">
+          <section class="footer__socialMedia">
+            <h2>¡Siguenos</h2>
+            <div class="footer__socialMedia__botones">
+              <a class="footer__socialMedia__botones__boton" href="#"></a>
+              <a class="footer__socialMedia__botones__boton" href="#"></a>
+              <a class="footer__socialMedia__botones__boton" href="#"></a>
+              <a class="footer__socialMedia__botones__boton" href="#"></a>
+            </div>
+            <h2>en las redes!</h2>
+          </section>
+
+          <section class="footer__elements">
+            <div class="footer__elements__column">
+              <h4>Título</h4>
+              <ul class="footer__elements__column__links">
+                <li><a>Elemento de footer</a></li>
+                <li><a>Elemento de footer</a></li>
+                <li><a>Elemento de footer</a></li>
+              </ul>
+            </div>
+          </section>
+          <section class="footer__elements">
+            <div class="footer__elements__column">
+              <h4>Título</h4>
+              <ul class="footer__elements__column__links">
+                <li><a>Elemento de footer</a></li>
+                <li><a>Elemento de footer</a></li>
+                <li><a>Elemento de footer</a></li>
+              </ul>
+            </div>
+          </section>
+          <section class="footer__elements">
+            <div class="footer__elements__column">
+              <h4>Título</h4>
+              <ul class="footer__elements__column__links">
+                <li><a>Elemento de footer</a></li>
+                <li><a>Elemento de footer</a></li>
+                <li><a>Elemento de footer</a></li>
+              </ul>
+            </div>
+          </section>
+          <section class="footer__elements">
+            <div class="footer__elements__column">
+              <h4>Título</h4>
+              <ul class="footer__elements__column__links">
+                <li><a>Elemento de footer</a></li>
+                <li><a>Elemento de footer</a></li>
+                <li><a>Elemento de footer</a></li>
+              </ul>
+            </div>
+          </section>
         </footer>
       `;
     }
--- public/components/header.js
+++ public/components/header.js
@@ -8,12 +8,51 @@ class Header extends HTMLElement
     connectedCallback() 
     {
       this.innerHTML = `
-        <style>
-          
-        </style>
-        <header>
-            <h1>Aqui va el header. Luego será actualizado.</h1>
-        </header>
+      <header class="header">
+        <section class="header__mitad">
+          <figure class="header__mitad__logo">
+            <img src="" alt="logo">
+            <h1>Shinsekai</h1>
+          </figure>
+  
+          <form class="header__mitad__searchbar" action="">
+            <select class="header__mitad__searchbar--filter" name="filtro" id="filtro">
+                <option value="1">heather</option>
+                <option value="2">jeader</option>
+                <option value="3">headher</option>
+                <option value="4">header</option>
+                <option value="5">jeadher</option>
+                <option value="6">jheader</option>
+                <option value="7">jheather</option>
+            </select>
+            <input class="header__mitad__searchbar--input" type="text" name="search" id="search">
+            <button class="header__mitad__searchbar--button" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+          </form>
+    
+          <nav class="header__mitad__buttons">
+            <a class="header__mitad__buttons__button" href="#"><img src="" alt="boton1"></a>
+            <a class="header__mitad__buttons__button" href="#"><img src="" alt="boton2"></a>
+            <a class="header__mitad__buttons__button" href="#"><img src="" alt="boton3"></a>
+          </nav>
+        </section>
+
+        <section class="header__mitad">
+          <ul class="header__mitad__categorias">
+            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
+            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
+            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
+            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
+            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
+            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
+            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
+            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
+            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
+            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
+            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
+          </ul>
+        </section>
+      </header>
+      <hr>
       `;
     }
   }
