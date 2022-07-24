17a29f2c7f3bb9870b2faf1166d3d85c275989ca
Julián <j.gakusei11@gmail.com>
2022-07-24 03:32:32 -0300

    CSS del header agregado. Correcciones realizadas a header.js

--- public/components/header.js
+++ public/components/header.js
@@ -10,13 +10,17 @@ class Header extends HTMLElement
       this.innerHTML = `
       <header class="header">
         <section class="header__mitad">
-          <figure class="header__mitad__logo">
-            <img src="" alt="logo">
+        <figure class="header__mitad__logo">
+          <i id="burger" class="fa-solid fa-bars"></i>
+          <a href="#">
+            <img src="/img/logo.png" alt="logo">
             <h1>Shinsekai</h1>
-          </figure>
+          </a>
+        </figure>
   
           <form class="header__mitad__searchbar" action="">
             <select class="header__mitad__searchbar--filter" name="filtro" id="filtro">
+              <option value="-1" disabled selected hidden>Filtro</option>
                 <option value="1">heather</option>
                 <option value="2">jeader</option>
                 <option value="3">headher</option>
@@ -25,34 +29,32 @@ class Header extends HTMLElement
                 <option value="6">jheader</option>
                 <option value="7">jheather</option>
             </select>
-            <input class="header__mitad__searchbar--input" type="text" name="search" id="search">
+            <input class="header__mitad__searchbar--input" type="text" name="search" id="search" placeholder="Buscar">
             <button class="header__mitad__searchbar--button" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
           </form>
     
           <nav class="header__mitad__buttons">
-            <a class="header__mitad__buttons__button" href="#"><img src="" alt="boton1"></a>
-            <a class="header__mitad__buttons__button" href="#"><img src="" alt="boton2"></a>
-            <a class="header__mitad__buttons__button" href="#"><img src="" alt="boton3"></a>
+            <a class="header__mitad__buttons__search"href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
+            <a class="header__mitad__buttons__button" href="#"><i class="fa-solid fa-heart"></i></a>
+            <a class="header__mitad__buttons__button" href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+            <a class="header__mitad__buttons__button" href="#"><i class="fa-solid fa-right-from-bracket"></i></a>
           </nav>
         </section>
 
-        <section class="header__mitad">
-          <ul class="header__mitad__categorias">
-            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
-            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
-            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
-            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
-            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
-            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
-            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
-            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
-            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
-            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
-            <li class="header__mitad__categorias--categoria"><a href="#">Categoría</a></li>
-          </ul>
+        <section class="header__categorias">
+          <a class="header__categorias--categoria" href="#">Categoría</a>
+          <a class="header__categorias--categoria" href="#">Categoría</a>
+          <a class="header__categorias--categoria" href="#">Categoría</a>
+          <a class="header__categorias--categoria" href="#">Categoría</a>
+          <a class="header__categorias--categoria" href="#">Categoría</a>
+          <a class="header__categorias--categoria" href="#">Categoría</a>
+          <a class="header__categorias--categoria" href="#">Categoría</a>
+          <a class="header__categorias--categoria" href="#">Categoría</a>
+          <a class="header__categorias--categoria" href="#">Categoría</a>
+          <a class="header__categorias--categoria" href="#">Categoría</a>
+          <a class="header__categorias--categoria" href="#">Categoría</a>
         </section>
       </header>
-      <hr>
       `;
     }
   }
