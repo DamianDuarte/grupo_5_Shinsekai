b50ac43026f37de9b030569640218ce01315d126
DamianDuarte <106710013+DamianDuarte@users.noreply.github.com>
2022-07-25 19:35:43 -0300

    medio footer done

--- public/components/footer.js
+++ public/components/footer.js
@@ -13,14 +13,13 @@ class Footer extends HTMLElement
           <section class="footer__socialMedia">
             <h2>¡Siguenos</h2>
             <div class="footer__socialMedia__botones">
-              <a class="footer__socialMedia__botones__boton" href="#"></a>
-              <a class="footer__socialMedia__botones__boton" href="#"></a>
-              <a class="footer__socialMedia__botones__boton" href="#"></a>
-              <a class="footer__socialMedia__botones__boton" href="#"></a>
+              <a class="footer__socialMedia__botones__boton" href="#"><i class="fa-brands fa-facebook-square"></i></a>
+              <a class="footer__socialMedia__botones__boton" href="#"><i class="fa-brands fa-twitter"></i></a>
+              <a class="footer__socialMedia__botones__boton" href="#"><i class="fa-brands fa-instagram"></i></i></a>
+              <a class="footer__socialMedia__botones__boton" href="#"><i class="fa-brands fa-tiktok"></i></a>
             </div>
             <h2>en las redes!</h2>
           </section>
-
           <section class="footer__elements">
             <div class="footer__elements__column">
               <h4>Título</h4>
@@ -29,30 +28,27 @@ class Footer extends HTMLElement
                 <li><a>Elemento de footer</a></li>
                 <li><a>Elemento de footer</a></li>
               </ul>
-            </div>
+              <hr class="footer__elements__colums__hr">
           </section>
           <section class="footer__elements">
-            <div class="footer__elements__column">
+              <h4>Título</h4>
+              <ul class="footer__elements__column__links">
+                <li><a>Elemento de footer</a></li>
+                <li><a>Elemento de footer</a></li>
+                <li><a>Elemento de footer</a></li> 
+              </ul>
+              <hr class="footer__elements__colums__hr">
+          </section>
+          <section class="footer__elements">
               <h4>Título</h4>
               <ul class="footer__elements__column__links">
                 <li><a>Elemento de footer</a></li>
                 <li><a>Elemento de footer</a></li>
                 <li><a>Elemento de footer</a></li>
               </ul>
-            </div>
+              <hr class="footer__elements__colums__hr">
           </section>
           <section class="footer__elements">
-            <div class="footer__elements__column">
-              <h4>Título</h4>
-              <ul class="footer__elements__column__links">
-                <li><a>Elemento de footer</a></li>
-                <li><a>Elemento de footer</a></li>
-                <li><a>Elemento de footer</a></li>
-              </ul>
-            </div>
-          </section>
-          <section class="footer__elements">
-            <div class="footer__elements__column">
               <h4>Título</h4>
               <ul class="footer__elements__column__links">
                 <li><a>Elemento de footer</a></li>
