27472878ada30e7435317039af7a9718b2ff28e4
DamianDuarte <damykirablack@gmail.com>
2022-10-13 18:53:45 -0300

    db product done

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -15,42 +15,5 @@ module.exports={
         const tags = dataParser.loadData('tags.json');
 
         return res.render('./products/payment', {products, categories, tags})
-    },
-    create: (req, res)=>{
-        const products = dataParser.loadData('products.json');
-        const categories = dataParser.loadData('categories.json');
-        const tags = dataParser.loadData('tags.json');
-
-        return res.render('./products/create', {products, categories, tags})
-    },
-    edit: (req, res)=>{
-        const products = dataParser.loadData('products.json');
-        const product = products.find(p => p.id === +req.params.id);
-        const categories = dataParser.loadData('categories.json');
-        const tags = dataParser.loadData('tags.json');
-
-        return res.render('./products/edit', {products, product, categories, tags})
-    }, 
-    update: (req, res)=>{
-        const products = dataParser.loadData('products.json');
-        const product = products.find(p => p.id === +req.params.id);
-        const categories = dataParser.loadData('categories.json');
-        const tags = dataParser.loadData('tags.json');
-
-        return res.render('./products/edit', {products, product, categories, tags})
-    },
-    delete: (req, res)=>{
-        const products = dataParser.loadData('products.json');
-        const categories = dataParser.loadData('categories.json');
-        const tags = dataParser.loadData('tags.json');
-
-        return res.render('./products/delete', {products, categories, tags})
-    },
-    destroy: (req, res)=>{
-        const products = dataParser.loadData('products.json');
-        const categories = dataParser.loadData('categories.json');
-        const tags = dataParser.loadData('tags.json');
-
-        return res.render('./products/delete', {products, categories, tags})
-    }  
+    }
 }
---
+++ src/sql/data.sql
@@ -0,0 +1,17 @@
+/*  PRODUCTS */
+insert into products (id, name, price, discount, description, views, category_id, sale_id) values 
+(1, 'Dc - Sakura Fu', 42, 50, 'felis ut at dolor quis odio consequat varius integer ac leo pellentesque ultrices mattis', 217, 2, 4)
+(2, 'Saskatoon Berries - Frozen', 128, 50, 'libero quis orci nullam molestie nibh in lectus pellentesque at nulla suspendisse potenti cras in purus eu magna', 461, 1, 1)
+(3, 'Sole - Iqf', 38, 0, 'mi integer ac neque duis bibendum morbi non quam nec', 382, 5, 4)
+(4, 'Syrup - Golden, Lyles', 193, 70, 'consectetuer adipiscing elit proin risus praesent lectus vestibulum quam sapien varius ut blandit non interdum in ante', 65, 5, 3)
+(5, 'Muffin Chocolate Individual Wrap', 93, 30, 'ut ultrices vel augue vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae', 155, 3, 3)
+(6, 'Marjoram - Dried, Rubbed', 119, 70, 'in quam fringilla rhoncus mauris enim leo rhoncus sed vestibulum sit amet cursus id turpis integer aliquet massa id lobortis', 325, 1, 2)
+(7, 'Emulsifier', 112, 50, 'eget rutrum at lorem integer tincidunt ante vel ipsum praesent blandit lacinia erat vestibulum sed magna at nunc commodo', 246, 5, 2)
+(8, 'Veal - Insides Provini', 83, 30, 'lorem integer tincidunt ante vel ipsum praesent blandit lacinia erat vestibulum sed magna at nunc commodo placerat', 181, 4, 5)
+(9, 'Veal - Chops, Split, Frenched', 141, 50, 'hac habitasse platea dictumst maecenas ut massa quis augue luctus tincidunt nulla', 217, 4, 5)
+(10, 'Noodles - Cellophane, Thin', 179, 50, 'congue eget semper rutrum nulla nunc purus phasellus in felis donec', 230, 1, 5)
+(11, 'Ice Cream Bar - Rolo Cone', 112, 90, 'iaculis congue vivamus metus arcu adipiscing molestie hendrerit at vulputate', 400, 5, 4)
+(12, 'Nantucket Orange Juice', 166, 70, 'metus sapien ut nunc vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae', 43, 4, 1)
+(13, 'Wine - Semi Dry Riesling Vineland', 64, 30, 'porttitor lacus at turpis donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit', 50, 5, 2)
+(14, 'Mushroom - Enoki, Fresh', 181, 90, 'maecenas tristique est et tempus semper est quam pharetra magna ac consequat metus sapien ut nunc vestibulum ante ipsum', 16, 2, 3)
+(15, 'Oil - Olive Bertolli', 159, 70, 'sit amet sapien dignissim vestibulum vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae nulla', 301, 5, 4);
