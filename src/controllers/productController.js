9b2b0f7dc02e5b8f2f1a8de072992d49ccbe6832
DamianDuarte <damykirablack@gmail.com>
2022-10-20 19:29:47 -0300

    arreglar create frustacion, desesperacion, desesperanza

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,29 +1,23 @@
-const dataParser = require('../data/dataParser');
 const db = require('../database/models');
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-module.exports={
-    detail: (req, res) => {
-		// Do the magic
-		db.Product.findByPk(req.params.id,{
-			include : [{all : true}]
-		})
-			.then(product => {
-				return res.render('detail', {
-			product,
-			toThousand
-			})
-			})
-			.catch(error => console.log(error))
-		
-	},
-    /* details: (req, res)=>{
-        const products = dataParser.loadData('products.json');
-        const product = products.find(p => p.id === +req.params.id);
-        const categories = dataParser.loadData('categories.json');
-        const tags = dataParser.loadData('tags.json');
+const { associations, filters, createError} = require('../helpers');
 
-        return res.render('./products/details', {products, product, categories, tags})
-    }, */
+module.exports={
+    details: async (req, res) => {
+        try {
+            const product = await db.products.findByPk(req.params.id, associations.get('images'))
+/*             const products = await db.products.findAll();  No tengo pensado usarte hasta que ande la pagina */
+            const categories = await db.categories.findAll();
+            const tags = await db.tags.findAll();
+
+            if(!product) throw createError(404, 'Producto no encontrado.')
+            
+            return res.render('./products/details', { product, categories, tags });
+        } catch (error) {
+            console.log(error);
+            return res.send(error);            
+        }
+	},
     payment: (req, res)=>{
         const products = dataParser.loadData('products.json');
         const categories = dataParser.loadData('categories.json');
@@ -32,52 +26,79 @@ module.exports={
         return res.render('./products/payment', {products, categories, tags})
     },
     /* CRUD */
-    list: (req, res)=>{
+    create : async(req,res)=>{
+        try {
+            const categories = await db.categories.findAll();
+            const tags = await db.tags.findAll();
+            const products = await db.products.findAll(associations.get('images', 'tags')); 
+            return res.render('createProducts', {categories, tags, products});
+        } catch (error) {
+            console.log(error);
+            return res.send(error);            
+        }
+    },
+    processCreate: async (req, res)=>{
+        try {
+            const product = await db.products.create(req.body);
+            return res.redirect('/product/details/' + product.id)
+        } catch (error) {
+            console.log(error);
+            return res.send(error);
+        }
+    },
+    edit: async (req, res)=>{
+        try {
+            const product = await db.products.findByPk(req.params.id, associations.get('images', 'tags'));
+            const products  = await db.products.findAll(associations.get('images'));
+            const categories = await db.categories.findAll();
+            const tags = await db.tags.findAll();
 
-        db.products.findAll()
-        .then(products => res.render('./products/details', {products}))
-        .catch(error => console.log(error))
+            console.log(product);
+            if(!product) throw createError(404, 'Producto no encontrado');
+
+            return res.render('editProducts', { product, products, categories, tags });
+        } catch (error) {
+            console.log(error);
+            return res.send(error);            
+        }
     },
-    create: (req, res)=>{
-        db.products.create({
-            name: req.body.name,
-            description: req.body.description,
-            price: req.body.price,
-            image: req.body.image,
-            category_id: req.body.category_id,
-            tag_id: req.body.tag_id
-        })
-        .then(()=> res.redirect('/products/details'))
-        .catch(error => console.log(error))
+    update: async (req, res)=>{
+        try {
+            const product = await db.products.findByPk(req.params.id, associations.get('images', 'tags'));
+            const products  = await db.products.findAll(associations.get('images'));
+            const categories = await db.categories.findAll();
+            const tags = await db.tags.findAll();
+
+            if(!product) throw createError(404, 'Producto no encontrado.')
+
+            await product.update({
+                name: req.body.name,
+                description: req.body.description,
+                price: req.body.price,
+                image: req.body.image,
+                category_id: req.body.category_id,
+                tag_id: req.body.tag_id
+            })
+
+            return res.redirect('/products/details/'+ product.id);
+        } catch (error) {
+            console.log(error);
+            return res.send(error);            
+        }
     },
-    edit: (req, res)=>{
-        db.products.findByPk(req.params.id)
-        .then(product => res.render('./products/edit', {product}))
-        .catch(error => console.log(error))
-    },
-    update: (req, res)=>{
-        db.products.update({
-            name: req.body.name,
-            description: req.body.description,
-            price: req.body.price,
-            image: req.body.image,
-            category_id: req.body.category_id,
-            tag_id: req.body.tag_id
-        }, {
-            where: {
-                id: req.params.id
-            }
-        })
-        .then(()=> res.redirect('/products'))
-        .catch(error => console.log(error))
-    },
-    destroy: (req, res)=>{
-        db.products.destroy({
-            where: {
-                id: req.params.id
-            }
-        })
-        .then(()=> res.redirect('/products'))
-        .catch(error => console.log(error))
+    destroy: async (req, res)=>{
+        try {
+            const product = await db.products.findByPk(req.params.id, associations.get('images', 'tags'));
+            const products  = await db.products.findAll(associations.get('images'));
+            const categories = await db.categories.findAll();
+            const tags = await db.tags.findAll();
+
+            await product.destroy();
+
+            return res.redirect('/');
+        } catch (error) {
+            console.log(error);
+            return res.send(error);            
+        }
     }
     }
