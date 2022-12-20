d2626e1137cd2afd2ac2eb10a138eeb201a717a4
Julián <j.gakusei11@gmail.com>
2022-12-20 16:42:06 -0300

    stuff

--- public/js/payment.js
+++ public/js/payment.js
@@ -2,6 +2,37 @@ const cartContainer = document.querySelector('#cartDetail')
 
 let content;
 
+function updateLinks()
+{
+    const imgPrev = document.querySelector('#productImg'); //! Element img para actualizar
+    const allLinks = document.querySelectorAll('.productLink'); //! Lista de links (al pasarles el mouse, cambiar imagen)
+
+
+    for (let i = 0; i < allLinks.length; i++) 
+    {
+        allLinks[i].addEventListener('mouseenter', (event) => //! Evento: Al pasar el mouse, hacer lo de aquí adentro.
+        {
+            console.log(event.target); //! log de prueba
+            //! Aca agrega el código para actualizar la imagen
+        });
+    }
+}
+
+function updateTotal()
+{
+    const prices = document.getElementsByClassName('totalPrices');
+    let totalPrices = 0;
+
+    if(prices && prices.length > 0)
+    {
+        for (let i = 0; i < prices.length; i++) {
+            totalPrices += +(prices[i].textContent.replace('$', '').trim());
+        }
+    }
+
+    document.querySelector('#finalPrice').textContent = "$" + totalPrices;
+}
+
 function decreaseQty(id)
 {
     removeqty(id);
@@ -15,6 +46,8 @@ function decreaseQty(id)
     total.textContent = "$" + (totalAmount - unitAmount).toFixed(2);
 
     if(quantity.textContent == 0) document.querySelector('#listProduct' + id).remove();
+
+    updateTotal();
 }
 
 function increaseQty(id)
@@ -28,6 +61,7 @@ function increaseQty(id)
     const unitAmount = +unit.textContent.replace('$', '').trim();
 
     total.textContent = "$" + (totalAmount + unitAmount).toFixed(2);
+    updateTotal();
 }
 
 function updateList(data)
@@ -39,7 +73,7 @@ function updateList(data)
         cartContainer.innerHTML += `
         <div class="payment__main__info__amount__list__product" id='listProduct${item.data.id}'>
             <div class="payment__main__info__amount__list__product__column">
-                <a href="http://localhost:4000/products/details/${item.data.id}"><h4>${item.data.name}</h4></a>
+                <a class="productLink" href="http://localhost:4000/products/details/${item.data.id}"><h4>${item.data.name}</h4></a>
                 <p class="desktop-cantidad">Cantidad</p>
                 <div class="amount">
                     <button onclick="decreaseQty(${item.data.id})">-</button>
@@ -53,11 +87,14 @@ function updateList(data)
             </div>
             <div class="payment__main__info__amount__list__product__column">
                 <h4 class="desktop-precio">Precio total</h4>
-                <p id="paymentListTotal${item.data.id}">$ ${((item.data.price - (item.data.price / 100 * item.data.discount)) * item.qty).toFixed(2)}</p>
+                <p class="totalPrices" id="paymentListTotal${item.data.id}">$ ${((item.data.price - (item.data.price / 100 * item.data.discount)) * item.qty).toFixed(2)}</p>
             </div>
         </div>
         `;
-    })
+    });
+
+    updateTotal();
+    updateLinks();
 }
 
 fetch('/api/MICArt').then(response =>
